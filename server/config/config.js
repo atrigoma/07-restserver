47895bbbb0c8f293291430cbb8c89a0534bb1698
TRIGO MARTIN ALVARO <n64400@gruposantander.es>
2020-03-06 16:56:42 +0100

    include environment variable MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,8 @@ if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://127.0.0.1:27017/coffee';
 }
 else{
-    urlDB = 'mongodb+srv://admin:gdzMB6QdaBlEPSTf@cluster0-jizs6.mongodb.net/coffee';
+    //urlDB = 'mongodb+srv://admin:gdzMB6QdaBlEPSTf@cluster0-jizs6.mongodb.net/coffee';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB= urlDB;
